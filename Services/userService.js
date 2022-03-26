23393e07e619bd0b7f4266a801f0df75930cd890
Zubair Khan <91844770+abrarzargai@users.noreply.github.com>
2022-03-26 16:43:07 +0500

    Update userService.js

--- Services/userService.js
+++ Services/userService.js
@@ -4,7 +4,7 @@ const argon2 = require('argon2');
 const nodemailer = require("nodemailer");
 var jwt = require('jsonwebtoken');
 const { v4: uuidv4 } = require('uuid');
-const ImgBase = 'http://54.226.251.96/images'
+const ImgBase = 'http://54.226.251.96/images/'
 //******Generatingg token****/
 
 const signToken = (user) => {
