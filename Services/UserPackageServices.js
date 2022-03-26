64d17f3f90215f7ee0010ddc09d6aec713b9ee90
Zubair Khan <91844770+abrarzargai@users.noreply.github.com>
2022-03-26 16:43:50 +0500

    Update UserPackageServices.js

--- Services/UserPackageServices.js
+++ Services/UserPackageServices.js
@@ -3,7 +3,7 @@ const catchAsync = require('../utils/catchAsync');
 const mongoose = require('mongoose')
 const ObjectId = mongoose.Types.ObjectId;
 const nodemailer = require("nodemailer");
-const ImgBase = 'http://54.226.251.96/images';
+const ImgBase = 'http://54.226.251.96/images/';
 /***************Services************/
 
 exports.Add = catchAsync(async (req, res, next) => {
